e83cf10c421fc681e30a66bf32d4afcd8dc3e56b
Mauricio <mauricioaguirre2490@gmail.com>
2023-09-26 09:14:27 -0500

    Migraciones y seeder echos

--- database/migrations/2023_09_26_135737_create_ruta_table.php
+++ database/migrations/2023_09_26_135737_create_ruta_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ruta', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('nombre');
+            $table->string('posX');
+            $table->string('posY');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ruta');
     }
 };
