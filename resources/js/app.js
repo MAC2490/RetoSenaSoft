4b2fc06daa20984029d45aa7cf68ea775f42f567
Dr.Uribe <yeisonl2036@gmail.com>
2023-09-26 11:47:59 -0500

    First Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+import Vue from 'vue';
+
+window.Vue = require('vue');
+
+//Lista de componentes
+
+Vue.component('index', require('./components/Index.vue').default);
+
+
+
+const app = new Vue({
+	el: '#app'
+})
