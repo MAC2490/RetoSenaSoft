d5a5a115b3f7fa436093543ae74202a401e744a2
Mauricio <mauricioaguirre2490@gmail.com>
2023-09-26 15:51:42 -0500

    Login incompleto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,11 @@
-import './bootstrap';
+require('./bootstrap');
 
 import Vue from 'vue';
 
 window.Vue = require('vue');
 
 //Lista de componentes
-
-Vue.component('index', require('./components/Index.vue').default);
-
-
+Vue.component('login', require('./components/Login.vue').default);
 
 const app = new Vue({
 	el: '#app'
