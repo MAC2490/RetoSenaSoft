0086c4f60824dec3ddcb6c106f10601b6678d482
Dr.Uribe <yeisonl2036@gmail.com>
2023-09-26 12:02:06 -0500

    Añadiendo index (Incompleto)

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+	<section>
+		<div>
+			<form>
+				<input-json-csv @input="handleFileUpload" accept=".json"/>
+				<button @click="processFile">Guardar archivo</button>
+			</form>
+		</div>
+	</section>
+</template>
+<script></script>
