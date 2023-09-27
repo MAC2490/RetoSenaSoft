64d614e0232ca55c579da8093c867c60b1713ef4
Mauricio <mauricioaguirre2490@gmail.com>
2023-09-27 14:42:37 -0500

    Terminar guardado de ruta

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,7 @@ window.Vue = require('vue');
 
 //Lista de componentes
 
-<<<<<<< HEAD
-Vue.component('index', require('./components/Index.vue').default);
 Vue.component('index-modulo', require('./components/IndexModulos.vue').default);
-
-Vue.component('index-carga-datos', require('./components/CargaDatos/Index.vue').default);
-=======
-Vue.component('index-modulo', require('./components/IndexModulos.vue').default);
->>>>>>> origin/dev2
 Vue.component('index-registro-rutas', require('./components/RegistroRutas/Index.vue').default);
 
 
